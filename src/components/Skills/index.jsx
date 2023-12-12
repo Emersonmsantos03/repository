1a6945515240cd4a30117be7b26b832c4326e858
Emer Moraes <emermoraes03@gmail.com>
2023-12-12 15:24:39 +0000

    Testando a remoção das barras

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -3,13 +3,13 @@ import './skills.css'
 let Skills = () =>{
 
     const hab = [
-        {id: '1', image: '/public/img/logocss.png'},
-        {id: '2', image: '/public/img/logofirebase.png'},
-        {id: '3', image: '/public/img/logohtml.png'},
-        {id: '4', image: '/public/img/logojava.png'},
-        {id: '5', image: '/public/img/logojs.png'},
-        {id: '6', image: '/public/img/logomysql.png'},
-        {id: '7', image: '/public/img/logoreact.png'},
+        {id: '1', image: 'public/img/logocss.png'},
+        {id: '2', image: 'public/img/logofirebase.png'},
+        {id: '3', image: 'public/img/logohtml.png'},
+        {id: '4', image: 'public/img/logojava.png'},
+        {id: '5', image: 'public/img/logojs.png'},
+        {id: '6', image: 'public/img/logomysql.png'},
+        {id: '7', image: 'public/img/logoreact.png'},
     ]
 
     return(
